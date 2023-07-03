0c9365a77c899be1712e97a520b038b2cd470c74
Lucas Santos <lucassantos.dsilv@gmail.com>
2023-07-03 15:57:49 -0300

    :sparkles: init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem impedit vero accusamus incidunt quae doloribus odit alias aut maiores consequatur. Numquam minus rerum error ipsa iure, quae facilis doloremque eligendi?</h1>
+    </div>
+  );
+}
