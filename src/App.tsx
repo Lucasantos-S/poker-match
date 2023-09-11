397b7194da2a0f5a993575a982802d45fcf48433
Lucas Santos <lucassantos.dsilv@gmail.com>
2023-09-11 13:45:24 -0300

    :sparkles: feat: path configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import MobileProfile from "./components/MobileProfile";
 import { BrowserRouter as Router } from "react-router-dom";
-import Routes from "./Routes";
+import Routes from "@/Routes";
 export default function App() {
   return (
     <div className="overflow-y-hidden min-w-[800px]">
